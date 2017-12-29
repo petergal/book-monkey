dff498632e7cc2604e736ab7a066431336d6b453
Peter Gal <petergal@pgMBP2017.local>
2017-12-29 16:49:31 +0100

    first iteration book-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bm-root',
-  templateUrl: './app.component.html',
-  styles: []
+  template: '<bm-book-list></bm-book-list>'
+  // templateUrl: './app.component.html',
+  // styles: []
 })
 export class AppComponent {
-  title = 'bm';
+  // title = 'bm';
 }
